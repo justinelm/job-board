f5acc14f3fa837195171ee0a8663e1b5d0c85e50
Justine Mojal <justine.mojal@globe.com.ph>
2023-04-13 13:57:30 +0800

    feat: add support for company assoc

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,7 +1,21 @@
-import { Job } from './db.js'
+import { Job, Company } from './db.js'
 
 export const resolvers = {
     Query: {
         jobs: () => Job.findAll()
+    },
+
+    Job: {
+        //parent obj, 
+        // company: (job) => {
+        //     console.log('job', job)
+        //     return Company.findById(companyId)
+        //     return {
+        //         id: 'fake id',
+        //         name: 'Fake Company',
+        //         description: 'Fake description'
+        //     }
+        // }
+        company: (job) => Company.findById(job.companyId)
     }
 }
